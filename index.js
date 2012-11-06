4ae17380dac23a4a0572f99ae9e2238ecd13e73e
Tommy Messbauer <tommy@vast.com>
2012-11-06 11:56:13 -0600

    export logger

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var mixdown = {};
 mixdown.Server = require('./lib/server');
 mixdown.App = require('./lib/app');
 mixdown.Config = require('./lib/config');
+mixdown.Logger = require('./lib/logger');
 
 module.exports = mixdown;
