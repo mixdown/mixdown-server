13ad8ccf4ee8914bb70982009a314df9cd7271d1
Tommy Messbauer <tommy@vast.com>
2012-11-06 11:53:48 -0600

    added declarative logger. version bump

---
+++ tests/logger.js
@@ -0,0 +1,32 @@
+var _ = require('lodash'),
+	VastLogger = require('../lib/logger'),
+	tap = require('tap'),
+	test = tap.test;
+
+global.logger = VastLogger.create({
+	"defaults": {
+		"handleExceptions": false,
+      	"json": true,
+      	"timestamp": true,
+      	"colorize": true
+	},
+	"transports": [{
+		"transport": "Console",
+		"options": {
+			"handleExceptions": true
+		}
+	}]
+});
+
+test("Test Console logger", function(t) {
+
+	t.ok(logger, "logger should have been created.");
+	t.equal(_.keys(logger.transports).length, 1, "logger should have one transport");
+
+	logger.info('If you see this then the test passed.');
+
+	t.end();
+});
+
+
+
