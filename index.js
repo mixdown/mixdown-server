8a032adc042b0aa856b7a1c5231d4aac9c9ed9dd
Tommy Messbauer <tommy@vast.com>
2013-06-25 10:47:46 -0500

    added external config and hotswap reload with couchdb connector

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ mixdown.App = require('./lib/app');
 mixdown.Config = require('./lib/config');
 mixdown.Logger = require('./lib/logger');
 mixdown.MainFactory = require('./lib/main');
+mixdown.CouchConfig = require('./lib/couchconfig.js');
 
 module.exports = mixdown;
